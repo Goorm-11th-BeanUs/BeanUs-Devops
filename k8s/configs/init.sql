8c04e5d5f6f2a60d25c05b604776a2339fd36b76
Keonyoung Shim <kyshim9004@gmail.com>
2024-10-31 21:36:22 +0900

    feat: add tables

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -16,7 +16,40 @@ CREATE TABLE `user` (
     `role` varchar(100),
     `created_at` datetime not null default CURRENT_TIMESTAMP,
     `updated_at` datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    INDEX idx_user (`id`, `token`, `role`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO user (`token`,`name`,`address`,`phone_number`,`role`) VALUES ('ThisIsToken', '스타벅스', '제주특별자치도 제주시', '010-1111-2222', '사장님');
+
+
+
+DROP TABLE IF EXISTS `collect_rule`;
+CREATE TABLE `collect_rule`
+(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `cafe_id` int(11) NOT NULL,
+    `weekday` int(10) NOT NULL,
+    `time` varchar(5) NOT NULL,
+    `amount` int(11) NOT NULL,
+    `created_at` datetime not null default CURRENT_TIMESTAMP,
+    `updated_at` datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    INDEX idx_collect_rule (`id`, `cafe_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `collect_transaction`;
+CREATE TABLE `collect_transaction`
+(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `cafe_id` int(11) NOT NULL,
+    `client_name` varchar(100) NOT NULL,
+    `time` datetime NOT NULL,
+    `amount` int(11) NOT NULL,
+    `status` varchar(100),
+    `created_at` datetime not null default CURRENT_TIMESTAMP,
+    `updated_at` datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    INDEX idx_collect_transaction (`id`, `cafe_id`, `time`, `status`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
