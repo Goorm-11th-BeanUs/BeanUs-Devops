e5f81af4cea4a8ab91bf73db29e1f4a30fe0c626
Keonyoung Shim <kyshim9004@gmail.com>
2024-10-31 22:06:01 +0900

    chore: add dummy

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -39,6 +39,13 @@ CREATE TABLE `collect_rule`
     INDEX idx_collect_rule (`id`, `cafe_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO collect_rule (`cafe_id`,`weekday`,`time`,`amount`,`position`) VALUES (10, 1, '09:00', 10, '문 밖');
+INSERT INTO collect_rule (`cafe_id`,`weekday`,`time`,`amount`,`position`) VALUES (10, 5, '09:00', 10, '문 밖');
+INSERT INTO collect_rule (`cafe_id`,`weekday`,`time`,`amount`,`position`) VALUES (11, 2, '09:00', 20, '기타');
+INSERT INTO collect_rule (`cafe_id`,`weekday`,`time`,`amount`,`position`) VALUES (12, 3, '09:00', 20, '직접 전달');
+INSERT INTO collect_rule (`cafe_id`,`weekday`,`time`,`amount`,`position`) VALUES (12, 4, '09:00', 20, '직접 전달');
+INSERT INTO collect_rule (`cafe_id`,`weekday`,`time`,`amount`,`position`) VALUES (12, 6, '09:00', 10, '직접 전달');
+
 
 DROP TABLE IF EXISTS `collect_transaction`;
 CREATE TABLE `collect_transaction`
@@ -54,3 +61,12 @@ CREATE TABLE `collect_transaction`
     PRIMARY KEY (`id`),
     INDEX idx_collect_transaction (`id`, `cafe_id`, `time`, `status`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (10, 'ABC 양로원', '2024-10-01T09:00:00.000000+09:00', 10, 'COMPLETED');
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (10, 'ABC 양로원', '2024-10-02T09:00:00.000000+09:00', 10, 'COMPLETED');
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (10, 'ABC 양로원', '2024-10-03T09:00:00.000000+09:00', 10, 'COMPLETED');
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (10, 'ABC 양로원', '2024-11-01T09:00:00.000000+09:00', 10, 'READY');
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (10, 'ABC 양로원', '2024-11-03T09:00:00.000000+09:00', 10, 'WAITING');
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (11, 'ABC 양로원', '2024-10-05T09:00:00.000000+09:00', 10, 'COMPLETED');
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (11, 'ABC 양로원', '2024-10-06T09:00:00.000000+09:00', 10, 'COMPLETED');
+INSERT INTO collect_transaction (`cafe_id`,`client_name`,`time`,`amount`,`status`) VALUES (12, 'ABC 양로원', '2024-11-07T09:00:00.000000+09:00', 10, 'WAITING');
